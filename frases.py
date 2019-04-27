0dfa9f02379a9584a5390e4bb8ea65419a43e1fa
Sebastián <thelopezepol@outlook.es>
2019-04-26 19:56:07 -0600

    Add files via upload

---
+++ frases.py
@@ -0,0 +1,16 @@
+########################################
+# Integrantes: Sebastián López Herrera y Daniel Sequeira Retana
+# Fecha creación: 26/04/2019 12:00
+# Ultima actualización: 26/04/2019 19:20
+# Version 0.1 - Pyhton 3.7.3
+########################################
+# Importación de Librerias
+
+
+# Definición de Funciones
+
+
+# Programa Principal
+
+
+# - FIN - #
