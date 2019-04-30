0b5acc958c66c3b3ecc5bf75949241434bfde619
TheLopezepoL <thelopezepol@outlook.es>
2019-04-30 09:35:02 -0600

    Update frases.py

--- frases.py
+++ frases.py
@@ -6,10 +6,26 @@
 ########################################
 # Importación de Librerias
 import requests
+import json
 
 
 # Definición de Funciones
+def verificarRed:
 
+
+
+def sacarFrases(pcan):
+    frases = []
+    while pcan > 0:
+        url = 'http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote'
+        respuesta = requests.get(url)
+        if respuesta.status_code == 200:
+            contenido = json.loads(respuesta.content.decode('utf-8'))
+        else:
+            contenido = 'Error de conexion: intente de nuevo'
+        frases.append(contenido)
+        pcan -= 1
+    return frases
 
 # Programa Principal
 
