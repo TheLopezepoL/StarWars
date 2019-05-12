7a4b29dded8834d2c1944eae229b47b204e4892b
TheLopezepoL <thelopezepol@outlook.es>
2019-05-12 12:07:42 -0600

    GUI

--- funciones.py
+++ funciones.py
@@ -9,6 +9,7 @@ import requests
 import json
 import re
 import ast
+import tkinter
 
 
 # Definición de Funciones
@@ -74,18 +75,92 @@ def eliminarFRep(pfrases):
 
 
 def crearCdA(pfrases):
-    ncod = 0
+    ncod = len(pfrases)
+    pfrases.reverse()
     for p in pfrases:
-        ncod += 1
         nom = p['nom'].upper()
         li = nom[0]
         lf = nom[len(nom)-1]
         cod = li + str(ncod).zfill(3) + '-' + lf
         p['cod'] = cod
+        ncod -= 1
+    pfrases.reverse()
     return pfrases
 
 
 # Programa Principal
+#
+from tkinter import *
+from tkinter import Text
+from tkinter import Tk
+from tkinter import ttk
+# # # raiz
+raiz = Tk()
+style = ttk.Style()
+style.theme_use('clam')
+raiz.title("Frases de Star Wars")
+raiz.config(bg="black")
+raiz.resizable(0, 0)
+raiz.iconbitmap('logo.ico')
+raiz.geometry("800x450")
+# fondo
+imagen = PhotoImage(file='fondo.png')
+fondo = Label(raiz, image=imagen).place(x=-11, y=-8)
+#
+# # # texto buscar
+tbus = Entry(fondo, bg='yellow') #LightGoldenrod1
+tbus.place(x=500, y=100)
+tbus.config(width='5', font=('Fixedsys', 23), bd=10, relief='ridge')
+# # # boton buscar
+fbus = Button(fondo, text='Buscar', bg='yellow', fg='Black', font="Fixedsys")
+fbus.place(x=610, y=98)
+fbus.config(width="15", height="2", bd=10, relief='ridge', cursor='hand2')
+# # # boton enviar xml
+fenv = Button(fondo, text='Enviar XML', bg='yellow', fg='Black', font='Fixedsys')
+fenv.place(x=497, y=188)
+fenv.config(width='29', height='2', bd=10, relief='ridge', cursor="hand2")
+###
+tdic = Entry(fondo, bg='black', fg="Yellow", bd=1, relief='flat')
+tdic.place(x=497, y=276)
+tdic.config(width='31', font=('Fixedsys', 10))
+# # # frame frases
+ffra = Frame(fondo, width=415, height=335, bg='black')
+ffra.place(x=50, y=50)
+# # # texto frases
+tfra = Text(ffra, width=50, height=23, bg='Black', fg='yellow', font='Fixedsys')
+tfra.grid(row=0, column=0,)
+tfra.config(bd=1, relief='flat')
+#
+print(style.element_options("Vertical.TScrollbar.thumb"))
+
+# configure the style
+style.configure("Vertical.TScrollbar", gripcount=0,
+                background="yellow", darkcolor="gold3", lightcolor="yellow2",
+                troughcolor="black", bordercolor="black", arrowcolor="black")
+#
+sfra = ttk.Scrollbar(ffra, command=tfra.yview, orient="vertical")
+sfra.grid(row=0, column=1, sticky='nsew')
+tfra.config(yscrollcommand=sfra.set)
+#
+# # # boton manual de usuario
+mdu = Button(fondo, text="-> Manual de Usuario <-", bg='black', fg='White', font='Fixedsys')
+mdu.pack(side="bottom", fill='x')
+mdu.config(cursor='hand2', bd=1, relief='flat')
+###
+raiz.mainloop()
+###
+
+
+if verificarRed():
+    frases = sacarFrases(5)  #El tiempo de respuesta puede fallar
+    frases = eliminarFRep(frases)
+    frases = sacarNombre(frases)
+    frases = crearCdA(frases)
+    print(len(frases))
+    print(frases)
+else:
+    print('Nel we')
+
 
 
 # - FIN - #
